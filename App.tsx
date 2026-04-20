5253f79326e667094e184329b99edb5fc4382749
raphael <raphael.furini@hotmail.com>
2026-04-20 15:20:57 -0300

    feat: react query adjustments

--- App.tsx
+++ App.tsx
@@ -3,17 +3,27 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { useColorScheme } from 'react-native';
 import { useMemo } from 'react';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'react-native-gesture-handler';
 import Navigation from '@/navigation';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function App() {
   const colorScheme = useColorScheme();
   const theme = useMemo(() => (colorScheme === 'dark' ? DarkTheme : DefaultTheme), [colorScheme]);
 
   return (
-    <SafeAreaProvider>
-      <Navigation theme={theme} />
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <Navigation theme={theme} />
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
